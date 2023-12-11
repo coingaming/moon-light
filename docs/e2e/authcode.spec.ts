34638863a76e260a2ffa3df9de955756d592eba8
Salvatore Criscione <not.salvatore@gmail.com>
2023-12-11 13:20:02 +0200

    MDS-858: More tests

--- docs/e2e/authcode.spec.ts
+++ docs/e2e/authcode.spec.ts
@@ -6,8 +6,10 @@ const MAX_DIFF_PIXEL_RATIO = 0.01
 const DEFAULT_TIMEOUT = 1000; // We wait max for 1 second for mounting
 
 test.beforeEach(async ({ page }, testInfo) => {
-    console.log(`Running ${testInfo.title}`);
     const example = testInfo.title?.split(':')?.[0] ?? "Default"
+    const title = testInfo.title?.split(':')?.[1] || ''
+    console.log(`Running ${example} | ${title}`);
+    
     await page.goto(`/client/authcode/${example}`)
     await page.waitForTimeout(DEFAULT_TIMEOUT)
 });
@@ -15,5 +17,27 @@ test.beforeEach(async ({ page }, testInfo) => {
 test('Default: should be visible in a short term', async ({ page }) => {
     await expect(page).toHaveScreenshot(`${COMPONENT_NAME}.png`, {
         maxDiffPixelRatio: MAX_DIFF_PIXEL_RATIO
+    })
+})
+
+test('Default: should exists 6 input in the DOM', async ({ page }) => {
+    const elementExists = await page.$$('div > input')
+    await expect(elementExists.length).toBe(6)
+})
+
+test('Default: user should be able to input in every box', async({ page, browser }) => {
+    const inputs = (await page.$$('div > input'))
+    await inputs[0].fill('1')
+    await inputs[1].fill('1')
+    await inputs[2].fill('1')
+    await inputs[3].fill('1')
+    await inputs[4].fill('1')
+    await inputs[5].fill('1')
+
+    await page.waitForTimeout(100)
+
+    inputs.forEach(async(_, index) => {
+        const input = await page.locator(`div > input:nth-child(${(index+1)})`)
+        await expect(input).toHaveValue('1')
     })
 })
