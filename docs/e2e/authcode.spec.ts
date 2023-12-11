e44ed090d7a4af18887e0ce796c4812896552de2
Salvatore Criscione <not.salvatore@gmail.com>
2023-12-11 15:26:31 +0200

    MDS-858 Authcode screenshots and better titles

--- docs/e2e/authcode.spec.ts
+++ docs/e2e/authcode.spec.ts
@@ -9,66 +9,66 @@ test.beforeEach(async ({ page }, testInfo) => {
     const example = testInfo.title?.split(':')?.[0] ?? "Default"
     const title = testInfo.title?.split(':')?.[1] || ''
     console.log(`Running ${example} | ${title}`);
-    
+
     await page.goto(`/client/authcode/${example}`)
     await page.waitForTimeout(DEFAULT_TIMEOUT)
 });
 
-test('Default: should be visible in a short term', async ({ page }) => {
+test('Default: should render and match screenshot', async ({ page }) => {
     await expect(page).toHaveScreenshot(`${COMPONENT_NAME}.png`, {
         maxDiffPixelRatio: MAX_DIFF_PIXEL_RATIO
     })
 })
 
-test('ErrorState: should be visible in a short term', async ({ page }) => {
+test('ErrorState: should render and match screenshot', async ({ page }) => {
     await expect(page).toHaveScreenshot(`${COMPONENT_NAME}-ErrorState.png`, {
         maxDiffPixelRatio: MAX_DIFF_PIXEL_RATIO
     })
 })
 
-test('AllowedCharacters: should be visible in a short term', async ({ page }) => {
+test('AllowedCharacters: should render and match screenshot', async ({ page }) => {
     await expect(page).toHaveScreenshot(`${COMPONENT_NAME}-AllowedCharacters.png`, {
         maxDiffPixelRatio: MAX_DIFF_PIXEL_RATIO
     })
 })
 
-test('CustomLength: should be visible in a short term', async ({ page }) => {
+test('CustomLength: should render and match screenshot', async ({ page }) => {
     await expect(page).toHaveScreenshot(`${COMPONENT_NAME}-CustomLength.png`, {
         maxDiffPixelRatio: MAX_DIFF_PIXEL_RATIO
     })
 })
 
-test('DifferentGaps: should be visible in a short term', async ({ page }) => {
+test('DifferentGaps: should render and match screenshot', async ({ page }) => {
     await expect(page).toHaveScreenshot(`${COMPONENT_NAME}-DifferentGaps.png`, {
         maxDiffPixelRatio: MAX_DIFF_PIXEL_RATIO
     })
 })
 
-test('HintMessage: should be visible in a short term', async ({ page }) => {
+test('HintMessage: should render and match screenshot', async ({ page }) => {
     await expect(page).toHaveScreenshot(`${COMPONENT_NAME}-HintMessage.png`, {
         maxDiffPixelRatio: MAX_DIFF_PIXEL_RATIO
     })
 })
 
-test('Password: should be visible in a short term', async ({ page }) => {
+test('Password: should render and match screenshot', async ({ page }) => {
     await expect(page).toHaveScreenshot(`${COMPONENT_NAME}-Password.png`, {
         maxDiffPixelRatio: MAX_DIFF_PIXEL_RATIO
     })
 })
 
-test('Placeholder: should be visible in a short term', async ({ page }) => {
+test('Placeholder: should render and match screenshot', async ({ page }) => {
     await expect(page).toHaveScreenshot(`${COMPONENT_NAME}-Placeholder.png`, {
         maxDiffPixelRatio: MAX_DIFF_PIXEL_RATIO
     })
 })
 
-test('WithAutoSubmit: should be visible in a short term', async ({ page }) => {
+test('WithAutoSubmit: should render and match screenshot', async ({ page }) => {
     await expect(page).toHaveScreenshot(`${COMPONENT_NAME}-WithAutoSubmit.png`, {
         maxDiffPixelRatio: MAX_DIFF_PIXEL_RATIO
     })
 })
 
-test('WithManualSubmit: should be visible in a short term', async ({ page }) => {
+test('WithManualSubmit: should render and match screenshot', async ({ page }) => {
     await expect(page).toHaveScreenshot(`${COMPONENT_NAME}-WithManualSubmit.png`, {
         maxDiffPixelRatio: MAX_DIFF_PIXEL_RATIO
     })
@@ -76,12 +76,13 @@ test('WithManualSubmit: should be visible in a short term', async ({ page }) => 
 
 
 test('Default: should exists 6 input in the DOM', async ({ page }) => {
-    const elementExists = await page.$$('div > input')
-    await expect(elementExists.length).toBe(6)
+    const inputs = await page.locator('div > input')
+    const input = await inputs.all()
+    await expect(input.length).toBe(6)
 })
 
-test('Default: user should be able to input in every box', async({ page }) => {
-    const inputs = (await page.$$('div > input'))
+test('Default: should be able to input in every box', async ({ page }) => {
+    const inputs = await (await page.locator('div > input')).all()
     await inputs[0].fill('1')
     await inputs[1].fill('1')
     await inputs[2].fill('1')
@@ -91,8 +92,19 @@ test('Default: user should be able to input in every box', async({ page }) => {
 
     await page.waitForTimeout(100)
 
-    inputs.forEach(async(_, index) => {
-        const input = await page.locator(`div > input:nth-child(${(index+1)})`)
+    inputs.forEach(async (_, index) => {
+        const input = await page.locator(`div > input:nth-child(${(index + 1)})`)
         await expect(input).toHaveValue('1')
     })
 })
+
+test('Default: should change focus when the user finish the input', async ({ page }) => {
+    // Get the first input
+    const input = await page.locator(`div > input:nth-child(1)`)
+    const secondInput = await page.locator(`div > input:nth-child(2)`)
+
+    await input.press('1')
+    await page.waitForTimeout(100)
+    await expect(secondInput).toBeFocused()
+})
+
