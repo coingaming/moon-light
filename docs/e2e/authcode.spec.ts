f362c94c25bae6e2dba386d1e03d1b2df7b45312
Salvatore Criscione <not.salvatore@gmail.com>
2023-12-11 12:49:19 +0200

    Optimized tests flow

--- docs/e2e/authcode.spec.ts
+++ docs/e2e/authcode.spec.ts
@@ -3,9 +3,16 @@ import { test, expect } from '@playwright/test'
 const COMPONENT_NAME = 'authcode'
 const MAX_DIFF_PIXEL_RATIO = 0.01
 
-test('should be visible in a short term', async({ page }) => {
-    await page.goto('/client/authcode/Default')
+const DEFAULT_TIMEOUT = 1000; // We wait max for 1 second for mounting
 
+test.beforeEach(async ({ page }, testInfo) => {
+    console.log(`Running ${testInfo.title}`);
+    const example = testInfo.title?.split(':')?.[0] ?? "Default"
+    await page.goto(`/client/authcode/${example}`)
+    await page.waitForTimeout(DEFAULT_TIMEOUT)
+});
+
+test('Default: should be visible in a short term', async ({ page }) => {
     await expect(page).toHaveScreenshot(`${COMPONENT_NAME}.png`, {
         maxDiffPixelRatio: MAX_DIFF_PIXEL_RATIO
     })
