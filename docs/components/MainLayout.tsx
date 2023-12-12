a63189857962297cbe8c8024a696af6be9cb86f7
Salvatore Criscione <not.salvatore@gmail.com>
2023-12-12 10:01:57 +0200

    MDS-858 Fixed homepage issue with new layout

--- docs/components/MainLayout.tsx
+++ docs/components/MainLayout.tsx
@@ -3,15 +3,18 @@ import { ReactNode } from "react";
 import Sidebar from "@/components/sidebar/Sidebar";
 import Footer from "@/components/footer/Footer";
 import Settings from "@/components/settings/Settings";
+interface IMainLayout {
+    children: ReactNode; isMockup?: boolean
+}
 
 export const MainLayout = (
-    {children, isMockup = false }: {children: ReactNode; isMockup: boolean}
+    { children, isMockup = false }: IMainLayout
 ) => {
     if (isMockup) return children;
     return <>
         <Sidebar />
         <main className="min-h-screen ms-80 bg-goku flex-1 flex flex-col rounded-ss-3xl px-5 xl:px-20 2xl:px-32 pt-12 xl:pb-52">
-          {children}
+            {children}
         </main>
         <Settings />
         <Footer />
