e815269c7f19a070768174f48b955ee1cc37ea22
Salvatore Criscione <not.salvatore@gmail.com>
2023-12-13 11:50:40 +0200

    MDS-858 AuthCode Remove webServer temp

--- docs/playwright.config.ts
+++ docs/playwright.config.ts
@@ -20,12 +20,12 @@ export default defineConfig({
 
   // Run your local dev server before starting the tests:
   // https://playwright.dev/docs/test-advanced#launching-a-development-web-server-during-the-tests
-  webServer: {
-    command: 'npm run dev',
-    url: baseURL,
-    timeout: 120 * 1000,
-    reuseExistingServer: !process.env.CI,
-  },
+  // webServer: {
+  //   command: 'npm run dev',
+  //   url: baseURL,
+  //   timeout: 120 * 1000,
+  //   reuseExistingServer: !process.env.CI,
+  // },
 
   use: {
     // Use baseURL so to make navigations relative.
