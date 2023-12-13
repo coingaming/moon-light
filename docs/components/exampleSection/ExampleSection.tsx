89ecee5894d7a06779c0ddb77650be318be175b3
Triin Kullisaar <95503530+tkullisaar@users.noreply.github.com>
2023-12-13 10:24:44 +0200

    add accordion tests

--- docs/components/exampleSection/ExampleSection.tsx
+++ docs/components/exampleSection/ExampleSection.tsx
@@ -13,7 +13,6 @@ type Props = {
   description?: JSX.Element;
   component: JSX.Element;
   code: string;
-  
 };
 
 export const ExampleSection = async ({
@@ -23,7 +22,10 @@ export const ExampleSection = async ({
   code,
 }: Props) => (
   <div className="flex flex-col gap-4 relative">
-    <HeaderSection title={title} description={description} />
+    <HeaderSection
+      title={title}
+      description={description ? <MDX markdown={description} /> : undefined}
+    />
     <div className="bg-gohan rounded-moon-i-sm overflow-hidden mt-2">
       <ComponentPreview component={component} />
       <CodePreview code={code} />
@@ -39,7 +41,7 @@ export async function withExamples(
     examples: Record<string, string>;
   },
   data: string[],
-  componentName: string,
+  componentName: string
 ) {
   const { descriptions, examples } = client;
 
@@ -51,7 +53,7 @@ export async function withExamples(
       () => import(`@/app/client/${componentName}/examples/${ex}`),
       {
         loading: () => <Loader />,
-      },
+      }
     );
     let title;
     if (descriptions?.[exampleDescriptionKey]) {
