d07c891b94863177a59c20de01e3674ef3b5712f
Salvatore Criscione <not.salvatore@gmail.com>
2023-12-18 15:48:46 +0200

    MDX Basic elements

--- docs/components/MDX.tsx
+++ docs/components/MDX.tsx
@@ -1,8 +1,33 @@
 import { MDXRemote, MDXRemoteProps } from "next-mdx-remote/rsc";
+import { ReactNode, ComponentProps, HTMLProps } from "react";
+
+const defaultComponents: ComponentProps<any> = {
+  a: (props: HTMLProps<HTMLAnchorElement>) => (
+    <a {...props} className="transition-colors underline hover:text-piccolo">
+      {props.children}
+    </a>
+  ),
+  strong: (props: HTMLProps<HTMLSpanElement>) => (
+    <span {...props} className="font-medium">
+      {props.children}
+    </span>
+  ),
+  ul: (props: HTMLProps<HTMLUListElement>) => (
+    <ul {...props} className="list-disc ps-8">
+      {props.children}
+    </ul>
+  ),
+};
 
 export function MDX({
   markdown,
   ...rest
 }: { markdown: string } & Omit<MDXRemoteProps, "source">) {
-  return <MDXRemote {...rest} source={markdown} />;
+  return (
+    <MDXRemote
+      {...rest}
+      source={markdown}
+      components={rest.components || defaultComponents}
+    />
+  );
 }
