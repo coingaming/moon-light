781f9db7fd68d999f42d6cb37a7b2be15086a7f2
Salvatore Criscione <not.salvatore@gmail.com>
2023-12-20 15:47:32 +0200

    RTL Test

--- docs/e2e/avatar.spec.ts
+++ docs/e2e/avatar.spec.ts
@@ -11,14 +11,32 @@ test.beforeEach(async ({ page }, testInfo) => {
   await page.goto(`/client/${COMPONENT_NAME}/${example}`);
   await page.waitForTimeout(PLAYWRIGHT_DEFAULT_TIMEOUT);
 });
-test.afterEach(async ({ page }) => {
-  // Cleanup from route
-  await page.close();
-});
+// test.afterEach(async ({ page }) => {
+//   // Cleanup from route
+//   await page.close();
+// });
 
 test("Default: should render and match screenshot", async ({ page }) => {
   await expect(page).toHaveScreenshot(`${COMPONENT_NAME}-Default.png`, {
     maxDiffPixelRatio: PLAYWRIGHT_MAX_DIFF_PIXEL_RATIO,
+  });
+});
+
+test("Default: should render and match screenshot in RTL", async ({ page }) => {
+  await page.evaluate(() => {
+    const htmlElement = document?.querySelector("html");
+    if (htmlElement) {
+      htmlElement.setAttribute("dir", "rtl");
+    } else {
+      throw new Error("RTLProvider error: html element was not found");
+    }
+  });
+  await page.waitForSelector("html[dir=rtl]");
+  await page.waitForTimeout(1000);
+  await expect(page).toHaveScreenshot(`${COMPONENT_NAME}-Rtl.png`, {
+    maxDiffPixelRatio: PLAYWRIGHT_MAX_DIFF_PIXEL_RATIO,
+    threshold: 0,
+    omitBackground: false,
   });
 });
 
