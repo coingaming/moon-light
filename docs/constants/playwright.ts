a85749135f9f7432c5022c3f3781d2190589e13b
Salvatore Criscione <not.salvatore@gmail.com>
2023-12-22 15:30:27 +0200

    refactor: Fine tuning the const (#43)
    This will cause a huge improvement on the execution time of the tests.
    
    Test does not seems to fail, apparently it works very good.
    
    Tests:
    
    avatar from 9s to 4.5s
    authcode from 20-30 seconds -> 11.5
    accordion from seconds 41s-> 25s
    
    This is just a quality of life commit finetuning for now, to save 30 seconds each time we run the tests locally during the development.

--- docs/constants/playwright.ts
+++ docs/constants/playwright.ts
@@ -1,7 +1,7 @@
 // Default DIFF Pixel
 export const PLAYWRIGHT_MAX_DIFF_PIXEL_RATIO = 0.01;
 // We wait max for .5 second for mounting
-export const PLAYWRIGHT_DEFAULT_TIMEOUT = 500;
+export const PLAYWRIGHT_DEFAULT_TIMEOUT = 100;
 // We check difference > 4px
 export const PLAYWRIGHT_MAX_DIFF_PIXEL = 4;
 // Acceptable perceived color difference between the same pixel in compared images, ranging from 0 (strict) and 1 (lax). "pixelmatch" comparator computes color difference in YIQ color space
