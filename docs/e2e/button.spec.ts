5916b97ab96b9d3ca7377210086e31d64fd6532e
salvatorecriscioneweb <salvatorecriscioneweb@users.noreply.github.com>
2023-12-28 08:54:02 +0000

    Prettified Code!

--- docs/e2e/button.spec.ts
+++ docs/e2e/button.spec.ts
@@ -14,49 +14,51 @@ test.afterEach(async ({ page }) => {
 });
 
 test.describe("ButtonAsLinkHTML tests", () => {
-  test('ButtonAsLinkHTML: should render and match screenshot', async ({ page }) => {
-      await expect(page).toHaveScreenshot(`button-ButtonAsLinkHTML.png`)
+  test("ButtonAsLinkHTML: should render and match screenshot", async ({
+    page,
+  }) => {
+    await expect(page).toHaveScreenshot(`button-ButtonAsLinkHTML.png`);
   });
 });
 
 test.describe("Variants tests", () => {
-  test('Variants: should render and match screenshot', async ({ page }) => {
-      await expect(page).toHaveScreenshot(`button-Variants.png`)
+  test("Variants: should render and match screenshot", async ({ page }) => {
+    await expect(page).toHaveScreenshot(`button-Variants.png`);
   });
 });
 
 test.describe("Sizes tests", () => {
-  test('Sizes: should render and match screenshot', async ({ page }) => {
-      await expect(page).toHaveScreenshot(`button-Sizes.png`)
+  test("Sizes: should render and match screenshot", async ({ page }) => {
+    await expect(page).toHaveScreenshot(`button-Sizes.png`);
   });
 });
 
 test.describe("Icons tests", () => {
-  test('Icons: should render and match screenshot', async ({ page }) => {
-      await expect(page).toHaveScreenshot(`button-Icons.png`)
+  test("Icons: should render and match screenshot", async ({ page }) => {
+    await expect(page).toHaveScreenshot(`button-Icons.png`);
   });
 });
 
 test.describe("FullWidth tests", () => {
-  test('FullWidth: should render and match screenshot', async ({ page }) => {
-      await expect(page).toHaveScreenshot(`button-FullWidth.png`)
+  test("FullWidth: should render and match screenshot", async ({ page }) => {
+    await expect(page).toHaveScreenshot(`button-FullWidth.png`);
   });
 });
 
 test.describe("Disabled tests", () => {
-  test('Disabled: should render and match screenshot', async ({ page }) => {
-      await expect(page).toHaveScreenshot(`button-Disabled.png`)
+  test("Disabled: should render and match screenshot", async ({ page }) => {
+    await expect(page).toHaveScreenshot(`button-Disabled.png`);
   });
 });
 
 test.describe("Animations tests", () => {
-  test('Animations: should render and match screenshot', async ({ page }) => {
-      await expect(page).toHaveScreenshot(`button-Animations.png`)
+  test("Animations: should render and match screenshot", async ({ page }) => {
+    await expect(page).toHaveScreenshot(`button-Animations.png`);
   });
 });
 
 test.describe("Multiline tests", () => {
-  test('Multiline: should render and match screenshot', async ({ page }) => {
-      await expect(page).toHaveScreenshot(`button-Multiline.png`)
+  test("Multiline: should render and match screenshot", async ({ page }) => {
+    await expect(page).toHaveScreenshot(`button-Multiline.png`);
   });
 });
