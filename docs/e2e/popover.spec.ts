b7cd43243086596ad4af8c4b6841857e8df8420b
Salvatore Criscione <not.salvatore@gmail.com>
2024-01-04 17:15:26 +0200

    Update popover.spec.ts

--- docs/e2e/popover.spec.ts
+++ docs/e2e/popover.spec.ts
@@ -1,12 +1,24 @@
 import { test, expect } from "@playwright/test";
 import { setupTest, setDarkTheme, setRtl } from "@/utils/playwrightHelpers";
 
-const COMPONENT_NAME = "alert";
+const COMPONENT_NAME = "popover";
 
 setupTest(COMPONENT_NAME);
 
-test.describe("Popover in Light Theme", () => {});
+test.describe("Popover in Light Theme", () => {
+  test.describe("Default tests", () => {
+    // ...
+  });
+});
 
-test.describe("Popover in Dark Theme", () => {});
+test.describe("Popover in Dark Theme", () => {
+  test.describe("Default tests", () => {
+    // ...
+  });
+});
 
-test.describe("Popover in RTL", () => {});
+test.describe("Popover in RTL", () => {
+  test.describe("Default tests", () => {
+    // ...
+  });
+});
