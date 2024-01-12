cd872a420b0fee97ad5564b57dd038a2c1de2a4d
Triin Kullisaar <95503530+tkullisaar@users.noreply.github.com>
2024-01-12 12:07:16 +0200

    Update docs/app/client/snackbar/examples/Customization.tsx
    Co-authored-by: Karl Kallavus <232199+karl-kallavus@users.noreply.github.com>

--- docs/app/client/snackbar/examples/Customization.tsx
+++ docs/app/client/snackbar/examples/Customization.tsx
@@ -3,7 +3,7 @@
 import { Snackbar, Button } from "@heathmont/moon-core-tw";
 import { useState, useCallback } from "react";
 
-const Example = () => {
+const Customization = () => {
   const [snackbar, setSnackbar] = useState("");
 
   const openSnackbarHandler = useCallback(
