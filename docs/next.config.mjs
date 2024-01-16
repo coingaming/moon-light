0f39140373e0b025866924119a59e5dd27056a75
Salvatore Criscione <not.salvatore@gmail.com>
2024-01-16 17:19:36 +0200

    fix: crash

--- docs/next.config.mjs
+++ docs/next.config.mjs
@@ -2,14 +2,14 @@
 const nextConfig = {
   rewrites: async () => {
     const COMPONENTS = (await import("./components.constants.mjs"))?.default;
-    const namesCamelCase = COMPONENTS.filter(
-      (name) => (name.match(/[A-Z]/)?.length || 0) !== 0,
-    ).map((name) => {
-      return {
-        source: `/client/${name.toLowerCase()}`,
-        destination: `/client/${name}`,
-      };
-    });
+    const namesCamelCase = Object.keys(COMPONENTS)
+      .filter((name) => (name.match(/[A-Z]/)?.length || 0) !== 0)
+      .map((name) => {
+        return {
+          source: `/client/${name.toLowerCase()}`,
+          destination: `/client/${name}`,
+        };
+      });
     return namesCamelCase;
   },
 };
