ba2e91cd666d7374d09c320d470c8ba24e7fe14f
Salvatore Criscione <not.salvatore@gmail.com>
2024-01-16 12:03:50 +0200

    fix: make redirect works

--- docs/next.config.mjs
+++ docs/next.config.mjs
@@ -1,16 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  redirects: async () => {
+  rewrites: async () => {
     const COMPONENTS = (await import("./components.constants.mjs"))?.default;
-    const namesCamelCase = COMPONENTS.filter((name) => /A-Z/.test(name)).map(
-      (name) => {
-        return {
-          source: `/client/${name.toLowerCase()}`,
-          destination: `/client/${name}`,
-          permanent: true,
-        };
-      },
-    );
+    const namesCamelCase = COMPONENTS.filter(
+      (name) => (name.match(/[A-Z]/)?.length || 0) !== 0,
+    ).map((name) => {
+      return {
+        source: `/client/${name.toLowerCase()}`,
+        destination: `/client/${name}`,
+      };
+    });
     return namesCamelCase;
   },
 };
