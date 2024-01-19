c2698f5e945fec0ae92bb38c12b6d56b1a01417d
Salvatore Criscione <not.salvatore@gmail.com>
2024-01-19 02:38:17 +0200

    feat: Fix case with redirects :suspect: (#94)
    * draft: Fix case with redirects :suspect:
    
    * merge master
    
    * refactor: Components used instead of getExamples()
    
    * refactor: package.json changes
    
    * fix: make redirect works
    
    * refactor: templating
    
    * prebuild components
    
    * fix: crash

--- docs/components/sidebar/Sidebar.tsx
+++ docs/components/sidebar/Sidebar.tsx
@@ -1,38 +1,31 @@
-import { getExamples } from "@/utils/getExamples";
 import SearchButton from "../search/SearchButton";
-import Link from "next/link";
 import SidebarItem from "./SidebarItem";
+import COMPONENTS from "@/components.constants.mjs";
 
 export default async function Sidebar() {
-  const { server, client } = await getExamples();
   return (
     <aside className="fixed z-10 top-0 h-screen flex flex-shrink-0 flex-col w-72 flex-grow gap-6 pt-24 pb-28 lg:pb-20 px-5 lg:px-8 bg-goku overflow-y-scroll border-e border-beerus">
       <div>
         <SearchButton />
       </div>
       <nav className="flex flex-col gap-6" aria-label="Sidebar">
-        {/* <div className="flex flex-col gap-2">
-          <p className="ps-2 text-moon-10-caption font-medium uppercase text-trunks">
-            Server components
-          </p>
-          <ul role="list" className="flex flex-col gap-1">
-            {Object.entries(server).map(([key, _value]) => (
-              <li key={key}>
-                <SidebarItem href={`/server/${key}`}>{key}</SidebarItem>
-              </li>
-            ))}
-          </ul>
-        </div> */}
         <div className="flex flex-col gap-2">
           <p className="ps-2 text-moon-10-caption font-medium uppercase text-trunks">
             Client components
           </p>
           <ul role="list" className="flex flex-col gap-1">
-            {Object.entries(client).map(([key, _value]) => (
-              <li key={key}>
-                <SidebarItem href={`/client/${key}`}>{key}</SidebarItem>
-              </li>
-            ))}
+            {Object.keys(COMPONENTS).map((key: string) => {
+              const data = COMPONENTS[key as keyof typeof COMPONENTS] as {
+                title: string;
+              };
+              return (
+                <li key={key}>
+                  <SidebarItem href={`/client/${key}`}>
+                    {data.title}
+                  </SidebarItem>
+                </li>
+              );
+            })}
           </ul>
         </div>
       </nav>
---
+++ docs/next.config.mjs
@@ -0,0 +1,17 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  rewrites: async () => {
+    const COMPONENTS = (await import("./components.constants.mjs"))?.default;
+    const namesCamelCase = Object.keys(COMPONENTS)
+      .filter((name) => (name.match(/[A-Z]/)?.length || 0) !== 0)
+      .map((name) => {
+        return {
+          source: `/client/${name.toLowerCase()}`,
+          destination: `/client/${name}`,
+        };
+      });
+    return namesCamelCase;
+  },
+};
+
+export default nextConfig;
