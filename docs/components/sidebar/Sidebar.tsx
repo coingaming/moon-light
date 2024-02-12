ddea6052a0520d5bed8e0bdd9ef2d10e664a85fe
Triin Kullisaar <95503530+tkullisaar@users.noreply.github.com>
2024-02-12 09:19:52 +0200

    docs: MDS-976 DocsPage, header and footer alignment with new design  (#112)
    * removing images, adjusting header and footer
    
    * addign mobile view
    
    * words update
    
    * cleanup
    
    * cleanup
    
    ---------
    
    Co-authored-by: Karl Kallavus <232199+karl-kallavus@users.noreply.github.com>

--- docs/components/sidebar/Sidebar.tsx
+++ docs/components/sidebar/Sidebar.tsx
@@ -2,33 +2,33 @@ import SearchButton from "../search/SearchButton";
 import SidebarItem from "./SidebarItem";
 import COMPONENTS from "@/components.constants.mjs";
 
-export default async function Sidebar() {
+export default function Sidebar() {
   return (
-    <aside className="fixed z-10 top-[4.5rem] h-screen flex flex-shrink-0 flex-col w-72 flex-grow gap-6 pt-6 pb-28 lg:pb-20 px-5 lg:px-8 bg-goku overflow-y-scroll border-e border-beerus">
-      <div>
+    <nav
+      className="lg:z-0 z-40 flex flex-col gap-6 fixed top-40 lg:top-[4.5rem] h-screen w-screen lg:w-72 bg-goku overflow-y-scroll lg:border-e lg:border-beerus px-4 py-5 lg:px-3 lg:py-6"
+      aria-label="Sidebar"
+    >
+      <div className="lg:hidden block z-50 fixed top-[4.5rem] start-0 w-screen h-[5.5rem] py-6 px-6 border-b border-beerus bg-goku">
         <SearchButton />
       </div>
-      <nav className="flex flex-col gap-6" aria-label="Sidebar">
-        <div className="flex flex-col gap-2">
-          <p className="ps-2 text-moon-10-caption font-medium uppercase text-trunks">
-            Client components
-          </p>
-          <ul role="list" className="flex flex-col gap-1">
-            {Object.keys(COMPONENTS).map((key: string) => {
-              const data = COMPONENTS[key as keyof typeof COMPONENTS] as {
-                title: string;
-              };
-              return (
-                <li key={key}>
-                  <SidebarItem href={`/client/${key}`}>
-                    {data.title}
-                  </SidebarItem>
-                </li>
-              );
-            })}
-          </ul>
-        </div>
-      </nav>
-    </aside>
+      <div className="flex flex-col gap-2">
+        <p className="ps-2 text-moon-10-caption font-medium uppercase text-trunks">
+          Client components
+        </p>
+        <ul role="list" className="flex flex-col gap-1">
+          {Object.keys(COMPONENTS).map((key: string) => {
+            const data = COMPONENTS[key as keyof typeof COMPONENTS] as {
+              title: string;
+            };
+            return (
+              <li key={key}>
+                <SidebarItem href={`/client/${key}`}>{data.title}</SidebarItem>
+              </li>
+            );
+          })}
+        </ul>
+      </div>
+    </nav>
+    // </aside>
   );
 }
