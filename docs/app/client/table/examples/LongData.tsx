8d45c3b6b0293b571604b0279d6914e0bfcad89b
moon-ds <moon-ds@users.noreply.github.com>
2024-02-13 12:24:08 +0000

    Prettified Code!

--- docs/app/client/table/examples/LongData.tsx
+++ docs/app/client/table/examples/LongData.tsx
@@ -7,113 +7,121 @@ import { ColumnDef } from "@tanstack/react-table";
 import React from "react";
 
 type DataTypeHelper = {
-  uuid: string,
-  user: string,
-  processTime: string,
-  client: string,
-  gameNameAndProvider: string,
-  amount: number,
-  currency: React.JSX.Element,
-  status: React.JSX.Element,
-}
+  uuid: string;
+  user: string;
+  processTime: string;
+  client: string;
+  gameNameAndProvider: string;
+  amount: number;
+  currency: React.JSX.Element;
+  status: React.JSX.Element;
+};
 
 const Example = () => {
-  const columns = React.useMemo<ColumnDef<{}, DataTypeHelper>[]>(() => [
-    {
-      id: 'operation',
-      header: () => 'Transactions',
-      footer: () => 'Transactions',
-      sticky: 'left',
-      columns: [
-        {
-          header: () => 'Transaction UUID',
-          footer: () => 'Transaction UUID',
-          accessorKey: 'uuid',
-          size: '150',
-        },
-        {
-          header: () => 'User & Supplier user',
-          footer: () => 'User & Supplier user',
-          accessorKey: 'user',
-          size: '150',
-        },
-      ],
-    },
-    {
-      id: 'info',
-      header: () => 'Info',
-      footer: () => 'Info',
-      columns: [
-        {
-          header: () => 'Process time',
-          accessorKey: 'processTime',
-          footer: () => 'Process time',
-        },
-        {
-          header: () => 'Client',
-          accessorKey: 'client',
-          footer: () => 'Client',
-        },
-        {
-          header: () => 'Game name & provider',
-          accessorKey: 'gameNameAndProvider',
-          footer: () => 'Game name & provider',
-        },
-        {
-          header: () => 'Amount',
-          accessorKey: 'amount',
-          footer: () => 'Amount',
-        },
-
-      ],
-    },
-    {
-      id: 'status',
-      header: () => 'Status',
-      footer: () => 'Status',
-      sticky: 'right',
-      columns: [
-        {
-          header: () => 'Currency',
-          footer: () => 'Currency',
-          accessorKey: 'currency',
-          cell: props => (props.getValue()),
-          size: '90',
-        },
-        {
-          header: () => 'Status',
-          footer: () => 'Status',
-          accessorKey: 'status',
-          cell: props => (props.getValue()),
-          size: '90',
-        },
-      ]
-    }
-  ], []);
+  const columns = React.useMemo<ColumnDef<{}, DataTypeHelper>[]>(
+    () => [
+      {
+        id: "operation",
+        header: () => "Transactions",
+        footer: () => "Transactions",
+        sticky: "left",
+        columns: [
+          {
+            header: () => "Transaction UUID",
+            footer: () => "Transaction UUID",
+            accessorKey: "uuid",
+            size: "150",
+          },
+          {
+            header: () => "User & Supplier user",
+            footer: () => "User & Supplier user",
+            accessorKey: "user",
+            size: "150",
+          },
+        ],
+      },
+      {
+        id: "info",
+        header: () => "Info",
+        footer: () => "Info",
+        columns: [
+          {
+            header: () => "Process time",
+            accessorKey: "processTime",
+            footer: () => "Process time",
+          },
+          {
+            header: () => "Client",
+            accessorKey: "client",
+            footer: () => "Client",
+          },
+          {
+            header: () => "Game name & provider",
+            accessorKey: "gameNameAndProvider",
+            footer: () => "Game name & provider",
+          },
+          {
+            header: () => "Amount",
+            accessorKey: "amount",
+            footer: () => "Amount",
+          },
+        ],
+      },
+      {
+        id: "status",
+        header: () => "Status",
+        footer: () => "Status",
+        sticky: "right",
+        columns: [
+          {
+            header: () => "Currency",
+            footer: () => "Currency",
+            accessorKey: "currency",
+            cell: (props) => props.getValue(),
+            size: "90",
+          },
+          {
+            header: () => "Status",
+            footer: () => "Status",
+            accessorKey: "status",
+            cell: (props) => props.getValue(),
+            size: "90",
+          },
+        ],
+      },
+    ],
+    [],
+  );
 
   const defaultColumn = {
     minSize: 10,
     size: 150,
-    maxSize: Number.MAX_SAFE_INTEGER
+    maxSize: Number.MAX_SAFE_INTEGER,
   };
 
   const makeData = (length: number) => {
-    return Array.from('_'.repeat(length)).map((_, index) => {
+    return Array.from("_".repeat(length)).map((_, index) => {
       return {
-        uuid: '84837d8ac654aa4689efa4649-84837d8ac654aa4689efa4649756454a5646545546d54f6546f546',
-        user: 'aleksandr@heathmonitoring.com',
-        processTime: '2023-09-19T14:31:46.105Z',
-        client: 'Bender (old) Coingaming',
-        gameNameAndProvider: 'Pragmatic Play',
+        uuid: "84837d8ac654aa4689efa4649-84837d8ac654aa4689efa4649756454a5646545546d54f6546f546",
+        user: "aleksandr@heathmonitoring.com",
+        processTime: "2023-09-19T14:31:46.105Z",
+        client: "Bender (old) Coingaming",
+        gameNameAndProvider: "Pragmatic Play",
         amount: 22.97,
-        currency: <Tag className="bg-gray-100 text-lg text-gray-600 max-w-fit">USD</Tag>,
-        status: <Tag className="bg-roshi-10 text-lg text-roshi max-w-fit">SUCCESS</Tag>,
+        currency: (
+          <Tag className="bg-gray-100 text-lg text-gray-600 max-w-fit">USD</Tag>
+        ),
+        status: (
+          <Tag className="bg-roshi-10 text-lg text-roshi max-w-fit">
+            SUCCESS
+          </Tag>
+        ),
       };
     });
   };
 
   const data = React.useMemo(() => makeData(40), []);
-  const textClip = 'clip' as ClipProps;
+  const textClip = "clip" as ClipProps;
 
   return (
     <Table
@@ -127,6 +135,6 @@ const Example = () => {
       withFooter={true}
     />
   );
-}
+};
 
 export default Example;
