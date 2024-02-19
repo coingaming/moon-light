65ac9706d07d8b10f4f659135eccd410cf5af7e0
dkireev <dmytro.kireiev@gmail.com>
2024-02-19 17:20:00 +0200

    docs: add main page [MDS-999]

--- docs/components/sidebar/Sidebar.tsx
+++ docs/components/sidebar/Sidebar.tsx
@@ -6,7 +6,7 @@ import COMPONENTS from "@/components.constants.mjs";
 export default function Sidebar() {
   return (
     <div
-      className="lg:z-0 z-40 flex flex-col gap-6 fixed top-40 lg:top-[4.5rem] h-screen w-screen lg:w-72 bg-goku overflow-y-scroll lg:border-e lg:border-beerus px-4 py-5 lg:px-3 lg:py-6"
+      className="lg:z-0 z-40 flex flex-col gap-6 top-40 lg:top-[4.5rem] h-screen w-screen lg:w-72 bg-goku overflow-y-scroll lg:border-e lg:border-beerus px-4 py-5 lg:px-3 lg:py-6"
       aria-label="Sidebar"
     >
       <div className="lg:hidden block z-50 fixed top-[4.5rem] start-0 h-[5.5rem] py-6 px-6 border-b border-beerus bg-goku">
@@ -16,7 +16,7 @@ export default function Sidebar() {
         <p className="ps-2 text-moon-10-caption font-medium uppercase text-trunks">
           Overview
         </p>
-        <ul className="flex flex-col gap-2">
+        <ul className="flex flex-col gap-2 mb-4">
           {navigation.map((item) => (
             <li key={item.name}>
               <SidebarItem href={item.href}>
