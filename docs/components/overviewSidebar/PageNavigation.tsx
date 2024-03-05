4a6b611ffc336c8de086d5eff9de6b4e73f8045d
Karl Kallavus <232199+karl-kallavus@users.noreply.github.com>
2024-03-05 09:47:05 +0100

    fix: the breakpoint for the right aside navigation panel has been fixed (#133)

--- docs/components/overviewSidebar/PageNavigation.tsx
+++ docs/components/overviewSidebar/PageNavigation.tsx
@@ -1,5 +1,5 @@
 const PageNavigation = ({ children }: { children: React.ReactElement[] }) => (
-  <aside className="fixed top-[72px] end-0 w-72 pt-4 px-6 bg-goku overflow-y-scroll border-s border-beerus hidden lg:block">
+  <aside className="fixed top-[72px] end-0 w-72 pt-4 px-6 bg-goku overflow-y-scroll border-s border-beerus hidden xl:block">
     <nav className="flex flex-col gap-2" aria-label="Page navigation">
       <p className="ps-2 text-moon-14 font-medium text-bulma">On this page</p>
       <ul className="flex flex-col gap-2">{children}</ul>
