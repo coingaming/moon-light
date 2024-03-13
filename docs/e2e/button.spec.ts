2bbdaac8f95d8855e3ef374522ccf4bc96f6c655
dkireev <dmytro.kireiev@gmail.com>
2024-03-13 17:34:11 +0200

    feat: update Button and Group tests [MDS-1030]

--- docs/e2e/button.spec.ts
+++ docs/e2e/button.spec.ts
@@ -6,37 +6,6 @@ const COMPONENT_NAME = "button";
 setupTest(COMPONENT_NAME);
 
 test.describe("Default tests", () => {
-  test("Default: should render and match screenshot", async ({ page }) => {
-    await expect(page).toHaveScreenshot(`button-Default.png`);
-  });
-
-  test("Default: should correctly make the click animation", async ({
-    page,
-  }) => {
-    const btn = await page.getByText("Default");
-    const btnPos = await btn.boundingBox();
-    await page.mouse.move((btnPos?.x || 0) + 10, (btnPos?.y || 0) + 5, {
-      steps: 10,
-    });
-    await page.mouse.down();
-    await page.waitForTimeout(200); // Wait the animation
-    const scale = await btn.evaluate((el) => {
-      return {
-        x: parseFloat(
-          window.getComputedStyle(el).getPropertyValue("--tw-scale-x"),
-        ),
-        y: parseFloat(
-          window.getComputedStyle(el).getPropertyValue("--tw-scale-y"),
-        ),
-      };
-    });
-    await expect(scale.x).toBe(0.9);
-    await expect(scale.y).toBe(0.9);
-    // Only on the default we check visually
-    await expect(page).toHaveScreenshot(`button-Default-Clicked.png`);
-    await page.mouse.up();
-  });
-
   test("DefaultWithClick: should run the callback on the click", async ({
     page,
   }) => {
@@ -335,21 +304,6 @@ test.describe("Multiline tests", () => {
 });
 
 test.describe("RTL Tests", () => {
-  test("Default: should render and match screenshot in RTL", async ({
-    page,
-  }) => {
-    await page.evaluate(() => {
-      const htmlElement = document?.querySelector("html");
-      if (htmlElement) {
-        htmlElement.setAttribute("dir", "rtl");
-      } else {
-        throw new Error("RTLProvider error: html element was not found");
-      }
-    });
-    await page.waitForSelector("html[dir=rtl]");
-    await expect(page).toHaveScreenshot(`button-Default-RTL-Customization.png`);
-  });
-
   test("ButtonAsLinkHTML: should render and match screenshot in RTL", async ({
     page,
   }) => {
