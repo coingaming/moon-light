9b3b12add4b4d276a5a7d06ce15661143fe8cf4f
Karl Kallavus <232199+karl-kallavus@users.noreply.github.com>
2024-03-14 17:11:32 +0000

    feat: new main page [MDS-1028] (#154)
    * feat: a version of the page with components
    
    * feat: a version of the homepage with components
    
    * Prettified Code!
    
    * fix(docs): incorrect definition of the enum type in props tables [MDS-1029] (#155)
    
    * fix: incorrect definition of the enum type in props tables
    
    * Prettified Code!
    
    * fix: a changeset has been appended
    
    * fix: the code has been rebuilt
    
    * Prettified Code!
    
    * chore: cleanup
    
    * Prettified Code!

--- docs/app/page.tsx
+++ docs/app/page.tsx
@@ -1,13 +1,13 @@
 import MainLayout from "@/components/mainLayout/MainLayout";
 import PageTitle from "@/components/mainPage/PageTitle";
-import Vision from "@/components/mainPage/Vision";
+import Review from "@/components/mainPage/Review";
 
 export default async function Home() {
   return (
     <MainLayout>
       <div className="flex flex-col gap-12">
         <PageTitle />
-        <Vision />
+        <Review />
       </div>
     </MainLayout>
   );
---
+++ docs/components/mainPage/ComponentDescription.tsx
@@ -0,0 +1,28 @@
+import React from "react";
+import { useGetExample } from "@/utils/useGetExample";
+
+function parseDescription(description?: string) {
+  try {
+    if (!description) {
+      return "";
+    }
+
+    // Split the description into pieces and return the final piece
+    const pieces = description.split(/[-]{2,}/g);
+    const finalPiece = pieces.pop()?.trim();
+
+    return finalPiece;
+  } catch (error) {
+    console.error("Error while parsing the description: ", error);
+    return "";
+  }
+}
+
+const ComponentDescription = async ({ component }: { component: string }) => {
+  const { description } = (await useGetExample(component)) as {
+    description?: string;
+  };
+  return <>{parseDescription(description)}</>;
+};
+
+export default ComponentDescription;
