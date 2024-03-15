ade08b1365b65723e484016822a7c5abacb5f117
Karl Kallavus <232199+karl-kallavus@users.noreply.github.com>
2024-03-15 10:51:51 +0000

    chore: hide components from the main page (#157)
    * chore: hide components from the main page
    
    * chore: add changeset

--- docs/components/mainPage/Review.tsx
+++ docs/components/mainPage/Review.tsx
@@ -1,5 +1,5 @@
-import ReviewCard from "./ReviewCard";
-import COMPONENTS from "@/components.constants.mjs";
+// import ReviewCard from "./ReviewCard";
+// import COMPONENTS from "@/components.constants.mjs";
 
 const Review = () => {
   return (
@@ -7,14 +7,15 @@ const Review = () => {
       <h2 className="text-moon-56 font-medium max-w-3xl text-bulma">
         Accelerate your development journey with moon components.
       </h2>
-      <div className="grid grid-cols-1 justify-items-center md:grid-cols-2 2xl:grid-cols-3 auto-rows-auto w-full gap-x-5 gap-y-5">
+      {/* TODO has to be optimized */}
+      {/* <div className="grid grid-cols-1 justify-items-center md:grid-cols-2 2xl:grid-cols-3 auto-rows-auto w-full gap-x-5 gap-y-5">
         {Object.keys(COMPONENTS).map((componentKey, index) => {
           const { title } = COMPONENTS[componentKey as keyof typeof COMPONENTS];
           return (
             <ReviewCard key={index} title={title} component={componentKey} />
           );
         })}
-      </div>
+      </div> */}
     </>
   );
 };
