daeb849b0927f237e28b4785b0bf1748cd8e92a4
dkireev <dmytro.kireiev@gmail.com>
2024-03-19 15:55:43 +0200

    fix: revert Combobox updates

--- packages/core/src/combobox/Combobox.tsx
+++ packages/core/src/combobox/Combobox.tsx
@@ -324,7 +324,7 @@ const Options = ({
       className={mergeClassnames(
         menuWidth
           ? menuWidth
-          : "w-full max-h-[18.75rem] py-2 px-1 my-1 rounded-moon-s-md box-border bg-goku shadow-moon-lg z-5 absolute",
+          : "w-full max-h-[18.75rem] py-2 px-1 my-1 rounded-moon-s-md box-border bg-goku shadow-moon-lg z-10 absolute",
         "overflow-y-auto focus:outline-none",
         className,
       )}
