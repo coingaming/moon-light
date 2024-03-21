dc26b6ffe440140d97fd1c5d33a150e561cda1bc
moon-ds <entwickler.js@gmail.com>
2024-03-21 11:46:00 +0500

    feat: the changelog total data file can be generated by the script

--- docs/scripts/buildChangelog.mjs
+++ docs/scripts/buildChangelog.mjs
@@ -2,8 +2,11 @@ import fs from "fs";
 import path from "path";
 import { readdir } from "node:fs/promises";
 import { readFromFile } from "./buildExamplesType.mjs";
+import { writeFile } from "fs/promises";
 
+const LEVEL_TAB = 2;
 const BASEDIR = path.join(path.resolve(), '../');
+const storeAs = 'docs/generated/changelog.ts';
 const searchFor = ['CHANGELOG.md'];
 const scanMap = [
   'docs/CHANGELOG.md',
@@ -51,12 +54,41 @@ const loadContents = async (filePaths) => {
 
 const pickHeader = (content) => content.replace(/^#\s([^\n]+).+/ms, "$1");
 
+const cropVersion = (content) => {
+  const version = content.replace(/^([\d\.]+).+/gms, "$1");
+  const pattern = new RegExp(`^${version}[^#]+`, 'gms');
+  const tailPart = content.replace(pattern, '');
+  return { version, tailPart };
+}
+
+const cropChangesType = (content) => {
+  return {
+    type: content.replace(/^([^\s]+).+$/gms, "$1").toLowerCase(),
+    description: content.split(/\n{2,}/gms).splice(1)[0]
+  };
+};
+
+const splitDescription = (content) => {
+  const parts = content.trim().split(/\n+/gms);
+  /** TODO: If necessary, make the parsing into tree */
+  return parts;
+}
+
+const splitChanges = (content) => {
+  const parts = content.split('### ').splice(1);
+  return parts.map((part) => {
+    const { type, description } = cropChangesType(part);
+    return {
+      type,
+      description: splitDescription(description)
+    }
+  });
+}
+
 const splitVersions = (parts) => {
   return parts.map((part) => {
-    const version = part.replace(/^([\d\.]+).+/gms, "$1");
-    const pattern = new RegExp(`^${version}[^#]+`, 'gms');
-    const changes = part.replace(pattern, '');
-    console.log(changes);
+    const { version, tailPart } = cropVersion(part);
+    const changes = splitChanges(tailPart);
     return {
       version,
       changes
@@ -74,12 +106,64 @@ const parseData = (content) => {
 
 const getData = async (filePaths) => {
   const contents = await loadContents(filePaths);
-  return contents.map((content) => parseData(content));
+  return contents.map((content, index) => {
+    const section = filePaths[index].replace(BASEDIR, '').replace(/\/[^\/]+$/, '');
+    return {[section]: parseData(content)} 
+  });
 }
+
+/**
+ * Places tab characters in structured text data
+ * @param {string} contents - data to arrange.
+ */
+const tabulate = (contents) => {
+  let level = 0;
+  const lines = contents.split('\n')
+    .map((line) => {
+      if (line.match(/[\}\]]/)) level--;
+      if (level > 0) line = "\t".repeat(level) + line;
+      if (line.match(/[\{\[]/)) level++;
+      return line;
+    });
+  return lines.join('\n');
+}
+
+/**
+ * Makes edits to the text data
+ * @param {string} contents - data to arrange.
+ */
+const arrangeText = (contents) => {
+  return tabulate(contents.split('","').join('",\n"')
+    .split('":"').join('": "')
+    .split('":{').join('": {')
+    .split('":[').join('": [')
+    .split('},').join('},\n')
+    .split("[").join("[\n")
+    .split("]").join("\n]")
+    .split("{").join("{\n")
+    .split("}").join("\n}"));
+};
+
+const prepareData = (data) => {
+  const contents = arrangeText(JSON.stringify(data));
+  return new Uint8Array(Buffer.from(`const changeLogs = ${contents};\nexport default changeLogs;\n`));
+};
+
+/**
+ * Stores prepared data into the specified file
+ */
+const storeDataSet = async (data) => {
+  const controller = new AbortController();
+  const { signal } = controller;
+  const stream = prepareData(data);
+  const storePath = path.join(BASEDIR, storeAs);
+  await writeFile(storePath, stream, { signal });
+};
 
 const buildChangelog = async () => {
   const selectedFiles = await retrieveDestinationSet();
-  console.log(await getData(selectedFiles));
+  const data = await getData(selectedFiles);
+  await storeDataSet(data);
 };
 
 buildChangelog();
