3328be8e431d84397c8009e9e7dac8825e093b80
Karl Kallavus <232199+karl-kallavus@users.noreply.github.com>
2024-03-25 11:59:08 +0000

    feat: release page & changelog data assembling [MDS-1018] (#164)
    * feat: the changelog data assembler has been initiated
    
    * feat: data parser development is in progress
    
    * feat: the changelog total data file can be generated by the script
    
    * fix: the tab function has been moved to a separate module
    
    * chore: cleanup
    
    * fix: the arrangeText handler has been removed
    
    * fix: the arrangeText handler at the component description builder has been updated
    
    * chore: formatting
    
    * fix: code rebuild and page generation
    
    * Prettified Code!
    
    * chore: types cleanup

---
+++ docs/generated/changelog.ts
@@ -0,0 +1,14 @@
+const changeLogs = {
+  docs: "# docs\n\n## 10.13.5\n\n### Patch Changes\n\n- bump version to 10.13.5\n- Updated dependencies\n  - @heathmont/moon-base-tw@10.13.5\n  - @heathmont/moon-cmdk-tw@10.13.5\n  - @heathmont/moon-core-tw@10.13.5\n  - @heathmont/moon-table-v8-tw@10.13.5\n  - @heathmont/moon-themes-tw@10.13.5\n",
+  "packages/base":
+    "# @heathmont/moon-base-tw\n\n## 10.13.5\n\n### Patch Changes\n\n- bump version to 10.13.5\n",
+  "packages/cmdk":
+    "# @heathmont/moon-cmdk-tw\n\n## 10.13.5\n\n### Patch Changes\n\n- bump version to 10.13.5\n- Updated dependencies\n  - @heathmont/moon-core-tw@10.13.5\n",
+  "packages/core":
+    "# @heathmont/moon-core-tw\n\n## 10.13.5\n\n### Patch Changes\n\n- bump version to 10.13.5\n",
+  "packages/table-v8":
+    "# @heathmont/moon-table-v8-tw\n\n## 10.13.5\n\n### Patch Changes\n\n- bump version to 10.13.5\n- Updated dependencies\n  - @heathmont/moon-core-tw@10.13.5\n",
+  "packages/themes":
+    "# @heathmont/moon-themes-tw\n\n## 10.13.5\n\n### Patch Changes\n\n- bump version to 10.13.5\n",
+};
+export default changeLogs;
