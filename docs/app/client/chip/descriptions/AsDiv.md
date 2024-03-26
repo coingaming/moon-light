1033103989a34831d05971beb5b1caa8ffa48750
yarema184 <yuliana.yarema@heathmont.net>
2024-03-26 11:33:52 +0200

    fix: MDS-926 Chip as polymorphic component

---
+++ docs/app/client/chip/descriptions/AsDiv.md
@@ -0,0 +1,5 @@
+---
+title: Chip as block element
+---
+
+Utilize the as prop to specify the chip as a DIV.
---
+++ docs/app/client/chip/examples/AsDiv.tsx
@@ -0,0 +1,11 @@
+"use client";
+
+import { Chip } from "@heathmont/moon-core-tw";
+
+const Default = () => (
+  <Chip className="border border-beerus" as="div">
+    I'm not a button
+  </Chip>
+);
+
+export default Default;
