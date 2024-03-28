28f06debdf342c7a53b24bfb1bcbec08f264b4f0
moon-ds <entwickler.js@gmail.com>
2024-03-28 18:21:34 +0500

    feat: the rightSidebar component has been added

---
+++ docs/components/sidebar/RightSidebar.tsx
@@ -0,0 +1,37 @@
+"use client";
+import { mergeClassnames } from "@heathmont/moon-core-tw";
+import React from "react";
+
+type RightSidebarProps = {
+  purpose?: string;
+  title: string;
+  className?: string;
+  children?: React.ReactNode[];
+};
+
+const RightSidebar = ({
+  purpose,
+  title,
+  className,
+  children,
+}: RightSidebarProps) => {
+  return (
+    <aside
+      className={mergeClassnames(
+        "fixed top-[72px] end-0 w-64 pt-6 px-3 bg-goku overflow-y-scroll border-s border-beerus hidden xl:block",
+        className
+      )}
+    >
+      <nav className="flex flex-col gap-5" aria-label={purpose}>
+        <p className="text-moon-10-caption font-medium uppercase text-trunks">
+          {title}
+        </p>
+        <ul className="flex flex-col gap-2">
+          {children}
+        </ul>
+      </nav>
+    </aside>
+  );
+}
+
+export default RightSidebar;
