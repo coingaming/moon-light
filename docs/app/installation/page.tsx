dc4617f3bfa0a7bbcb759c788c5c586d901b7328
dkireev <dmytro.kireiev@gmail.com>
2024-04-30 14:33:10 +0300

    fix: remove unnesessary <> [MDS-1065]

--- docs/app/installation/page.tsx
+++ docs/app/installation/page.tsx
@@ -4,12 +4,10 @@ import installation from "@/generated/installation";
 
 export default async function InstallationPage() {
   return (
-    <>
-      <OverviewPage className="gap-12" title="Installation">
-        <div className="flex flex-col gap-6">
-          <MDX markdown={installation} />
-        </div>
-      </OverviewPage>
-    </>
+    <OverviewPage className="gap-12" title="Installation">
+      <div className="flex flex-col gap-6">
+        <MDX markdown={installation} />
+      </div>
+    </OverviewPage>
   );
 }
