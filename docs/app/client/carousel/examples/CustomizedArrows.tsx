2e4bc706b82e8995ae5130260e1a68a6af9af35d
MarioGranada <MarioGranada@users.noreply.github.com>
2024-09-30 13:47:11 +0000

    Prettified Code!

--- docs/app/client/carousel/examples/CustomizedArrows.tsx
+++ docs/app/client/carousel/examples/CustomizedArrows.tsx
@@ -13,46 +13,44 @@ const Example = () => {
   const items = Array.from({ length: 25 }, (index) => index);
   return (
     <div className="flex flex-col w-full items-center">
-
-    
-    <Carousel scrollTo={5} className="w-full sm:max-w-auto">
-      {({
-        scrollLeftToStep,
-        scrollRightToStep,
-        canScrollLeft,
-        canScrollRight,
-      }: RenderProps) => (
-        <>
-          <Button
-            onClick={scrollLeftToStep}
-            disabled={!canScrollLeft}
-            className="my-4"
-            data-testid="scroll-left"
-          >
-            Scroll to left
-          </Button>
-          <Carousel.Reel>
-            {items.map((_, index) => (
-              <Carousel.Item
-                key={index}
-                className="w-full max-w-80 h-48 border border-beerus"
-                data-testid={`carousel-${index}`}
-              >
-                {index}
-              </Carousel.Item>
-            ))}
-          </Carousel.Reel>
-          <Button
-            onClick={scrollRightToStep}
-            disabled={!canScrollRight}
-            className="my-4"
-            data-testid="scroll-right"
-          >
-            Scroll to Right
-          </Button>
-        </>
-      )}
-    </Carousel>
+      <Carousel scrollTo={5} className="w-full sm:max-w-auto">
+        {({
+          scrollLeftToStep,
+          scrollRightToStep,
+          canScrollLeft,
+          canScrollRight,
+        }: RenderProps) => (
+          <>
+            <Button
+              onClick={scrollLeftToStep}
+              disabled={!canScrollLeft}
+              className="my-4"
+              data-testid="scroll-left"
+            >
+              Scroll to left
+            </Button>
+            <Carousel.Reel>
+              {items.map((_, index) => (
+                <Carousel.Item
+                  key={index}
+                  className="w-full max-w-80 h-48 border border-beerus"
+                  data-testid={`carousel-${index}`}
+                >
+                  {index}
+                </Carousel.Item>
+              ))}
+            </Carousel.Reel>
+            <Button
+              onClick={scrollRightToStep}
+              disabled={!canScrollRight}
+              className="my-4"
+              data-testid="scroll-right"
+            >
+              Scroll to Right
+            </Button>
+          </>
+        )}
+      </Carousel>
     </div>
   );
 };
--- docs/app/client/carousel/examples/Indicators.tsx
+++ docs/app/client/carousel/examples/Indicators.tsx
@@ -10,28 +10,26 @@ const Example = () => {
   const items = Array.from({ length: 5 }, (index) => index);
   return (
     <div className="flex flex-col w-full items-center">
-
-    
-    <Carousel step={1} selectedIndex={1} className="w-full max-w-80">
-      <Carousel.LeftArrow data-testid="scroll-left">
-        <ControlsChevronLeftSmall />
-      </Carousel.LeftArrow>
-      <Carousel.Reel>
-        {items.map((_, index) => (
-          <Carousel.Item
-            key={index}
-            className="w-full max-w-80 h-48 border border-beerus"
-            data-testid={`carousel-${index}`}
-          >
-            {index}
-          </Carousel.Item>
-        ))}
-      </Carousel.Reel>
-      <Carousel.RightArrow data-testid="scroll-right">
-        <ControlsChevronRightSmall />
-      </Carousel.RightArrow>
-      <Carousel.Indicators />
-    </Carousel>
+      <Carousel step={1} selectedIndex={1} className="w-full max-w-80">
+        <Carousel.LeftArrow data-testid="scroll-left">
+          <ControlsChevronLeftSmall />
+        </Carousel.LeftArrow>
+        <Carousel.Reel>
+          {items.map((_, index) => (
+            <Carousel.Item
+              key={index}
+              className="w-full max-w-80 h-48 border border-beerus"
+              data-testid={`carousel-${index}`}
+            >
+              {index}
+            </Carousel.Item>
+          ))}
+        </Carousel.Reel>
+        <Carousel.RightArrow data-testid="scroll-right">
+          <ControlsChevronRightSmall />
+        </Carousel.RightArrow>
+        <Carousel.Indicators />
+      </Carousel>
     </div>
   );
 };
