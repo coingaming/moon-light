e551ffc99400ad659c90dc8f10a7ef84c93948ee
Roberto Angulo <roberto.angulo@yolo.com>
2024-12-20 17:06:49 +0200

    feat: add docs to stories [MDS-1442]

--- docs/app/stories/Avatar.stories.tsx
+++ docs/app/stories/Avatar.stories.tsx
@@ -3,9 +3,19 @@ import type { Meta, StoryObj } from "@storybook/react";
 
 const meta: Meta<typeof AvatarComponent> = {
   component: AvatarComponent,
+  title: "Moon DS/Avatar",
+  tags: ["autodocs"],
+  argTypes: {
+    size: {
+      options: ["xs", "sm", "md", "lg", "xl", "2xl"],
+      control: { type: "select" },
+      description: "Determines the size of the avatar.",
+    },
+  },
 };
 
 export default meta;
+
 type Story = StoryObj<typeof AvatarComponent>;
 
 export const Avatar: Story = {
--- docs/app/stories/Chip.stories.tsx
+++ docs/app/stories/Chip.stories.tsx
@@ -1,21 +1,40 @@
 import { Chip as ChipComponent } from "@heathmont/moon-core-tw";
-
 import type { Meta, StoryObj } from "@storybook/react";
 
 const meta: Meta<typeof ChipComponent> = {
   component: ChipComponent,
+  title: "Moon DS/Chip",
+  tags: ["autodocs"],
+  argTypes: {
+    isActive: {
+      control: { type: "boolean" },
+      description: "Determines whether the chip is active.",
+    },
+    size: {
+      options: ["sm", "md"],
+      control: { type: "select" },
+      description: "Sets the size of the chip.",
+    },
+    isStroke: {
+      control: { type: "boolean" },
+      description: "Adds a border stroke to the chip when set to true.",
+    },
+    children: {
+      control: "text",
+      description: "Content to display inside the chip.",
+    },
+  },
 };
 
 export default meta;
+
 type Story = StoryObj<typeof ChipComponent>;
 
-const args = {
-  isActive: true,
-  size: "md",
-  isStroke: true,
-  children: <span>To the moon</span>,
-};
-
 export const Chip: Story = {
-  args,
+  args: {
+    isActive: true,
+    size: "md",
+    isStroke: true,
+    children: "To the moon",
+  },
 };
