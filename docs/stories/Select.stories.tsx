3ecc16f9e2d8cf3a7f34b55859884eac9bb32500
MarioGranada <mario.hernandez@yolo.com>
2025-01-14 16:13:10 +0200

    feat: MDS-1473 add native select props

--- docs/stories/Select.stories.tsx
+++ docs/stories/Select.stories.tsx
@@ -9,19 +9,48 @@ const meta: Meta<NativeSelectType> = {
   component: NativeSelectComponent,
   argTypes: {
     size: {
+      description: "Size of the Select",
+      table: {
+        type: {},
+        defaultValue: { summary: "md" },
+      },
+      control: { type: "select" },
+      options: ["sm", "md", "lg"],
+    },
+    disabled: {
+      description: "Select disabled",
+      type: "boolean",
+      table: {
+        defaultValue: { summary: "false" },
+      },
+    },
+    readOnly: {
+      description: "Select read only",
+      type: "boolean",
+      table: {
+        defaultValue: { summary: "false" },
+      },
+    },
+    error: {
+      description: "Select error",
+      type: "boolean",
+      table: {
+        defaultValue: { summary: "false" },
+      },
+    },
+    className: {
+      description: "Additional CSS class for the select.",
       type: "string",
     },
   },
-  render: () => {
+  render: ({ size, ...args }) => {
     return (
-      <>
-        <NativeSelectComponent>
-          <option value="Italy">Italy</option>
-          <option value="Spain">Spain</option>
-          <option value="China">China</option>
-          <option value="Germany">Germany</option>
-        </NativeSelectComponent>
-      </>
+      <NativeSelectComponent size={size} {...args}>
+        <option value="Italy">Italy</option>
+        <option value="Spain">Spain</option>
+        <option value="China">China</option>
+        <option value="Germany">Germany</option>
+      </NativeSelectComponent>
     );
   },
 };
@@ -33,5 +62,9 @@ type Story = StoryObj<NativeSelectType>;
 export const Select: Story = {
   args: {
     size: "md",
+    disabled: false,
+    error: false,
+    className: "",
+    readOnly: false,
   },
 };
