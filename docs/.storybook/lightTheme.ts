0c9dedcdd710da7c630cbe7fcccc4e63bd60ae7e
MarioGranada <mario.hernandez@yolo.com>
2025-01-29 18:25:13 +0200

    feat: MDS-1561 replace storybook theme with moon DS related theme & content

---
+++ docs/.storybook/lightTheme.ts
@@ -0,0 +1,12 @@
+import { create } from "@storybook/theming";
+
+export default create({
+  base: "light",
+  fontBase: '"DM Sans", sans-serif',
+  brandTitle: "Moon UI Storybook",
+  brandUrl: "https://moon-ui.t2.testenv.io/",
+  brandImage:
+    "https://s3.us-east-1.amazonaws.com/cdn.coingaming.io/moon/products/moondesignsystem.png",
+  brandTarget: "_self",
+  textColor: "rgb(0,0,0)",
+});
