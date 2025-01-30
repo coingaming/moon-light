eb0c7f863b24fd1df5a52afb05943bb82a2f146d
MarioGranada <mario.hernandez@yolo.com>
2025-01-30 10:38:23 +0200

    feat: MDS-1561 minor change

--- docs/.storybook/lightTheme.ts
+++ docs/.storybook/lightTheme.ts
@@ -3,8 +3,8 @@ import { create } from "@storybook/theming";
 export default create({
   base: "light",
   fontBase: '"DM Sans", sans-serif',
-  brandTitle: "Moon UI Storybook",
-  brandUrl: "https://moon-ui.t2.testenv.io/",
+  brandTitle: "Moon React Storybook",
+  brandUrl: "https://moon.io/stories",
   brandImage:
     "https://s3.us-east-1.amazonaws.com/cdn.coingaming.io/moon/products/moondesignsystem.png",
   brandTarget: "_self",
