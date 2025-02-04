74653ea14c0ed3978823d80a06862dad9ed90b4c
Roberto Angulo <roberto.angulo@yolo.com>
2025-02-04 14:07:08 +0200

    fix: put back specific types getDefaultValues [MDS-1547]

--- docs/stories/utils/getDefaultValues.ts
+++ docs/stories/utils/getDefaultValues.ts
@@ -1,8 +1,8 @@
 import React from "react";
 
 const getDefaultValues = (
-  obj: Record<string, unknown>,
-  defaultValues: Record<string, unknown>,
+  obj: Record<string, string | boolean | number | React.ReactNode | undefined>,
+  defaultValues: Record<string, string | boolean | number>,
 ) => {
   const obj2 = Object.entries(obj).reduce((acc, [key, value]) => {
     return value !== defaultValues[key] ? { ...acc, [key]: value } : acc;
