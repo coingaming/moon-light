4a586b99a3745a28bf2db4f8e0c07141bc73779d
Roberto Angulo <roberto.angulo@yolo.com>
2025-02-04 13:24:35 +0200

    fix: set values to unknown [MDS-1547]

--- docs/stories/utils/getDefaultValues.ts
+++ docs/stories/utils/getDefaultValues.ts
@@ -1,8 +1,8 @@
 import React from "react";
 
 const getDefaultValues = (
-  obj: Record<string, string | boolean | number | React.ReactNode | undefined>,
-  defaultValues: Record<string, string | boolean | number>,
+  obj: Record<string, unknown>,
+  defaultValues: Record<string, unknown>,
 ) => {
   const obj2 = Object.entries(obj).reduce((acc, [key, value]) => {
     return value !== defaultValues[key] ? { ...acc, [key]: value } : acc;
