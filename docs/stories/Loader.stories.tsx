26b1c20a8947797987c385088220e331e9f6d349
robb91-dev <roberto.angulo@yolo.com>
2025-02-05 15:36:55 +0200

    feat: add Loader story [MDS-1548] (#245)
    * feat: add Loader story [MDS-1548]
    
    * feat: add changeset [MDS-1548]
    
    * fix: add default values [MDS-1548]

---
+++ docs/stories/Loader.stories.tsx
@@ -0,0 +1,62 @@
+import { Loader as LoaderComponent } from "@heathmont/moon-core-tw";
+import { Meta } from "@storybook/react";
+import React from "react";
+import getDefaultValues from "./utils/getDefaultValues";
+
+const defaultProps = {
+  color: "border-hit",
+  size: "md",
+  ariaLabel: "Loading",
+};
+
+const meta: Meta<typeof LoaderComponent> = {
+  title: "Moon DS/Loader",
+  component: LoaderComponent,
+  tags: ["autodocs"],
+  argTypes: {
+    color: {
+      type: "string",
+      defaultValue: "border-hit",
+      description: "Set the border color of the loader",
+      table: {
+        type: {
+          summary: "string",
+        },
+      },
+    },
+    size: {
+      table: {
+        type: {
+          summary: "sm | md | lg",
+        },
+      },
+      type: "string",
+      control: { type: "select" },
+      options: ["sm", "md", "lg"],
+      description: "Set the loader size",
+    },
+    ariaLabel: {
+      control: "text",
+      defaultValue: "Loading",
+      description: "Accessibility label for the loader",
+      table: {
+        type: {
+          summary: "string",
+        },
+      },
+    },
+  },
+  render: ({ color, size, ariaLabel }) => {
+    const rootProps = getDefaultValues(
+      { color, size, ariaLabel },
+      defaultProps,
+    );
+    return <LoaderComponent {...rootProps} />;
+  },
+};
+
+export default meta;
+
+export const Loader = {
+  args: defaultProps,
+};
