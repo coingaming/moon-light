614ba1db728cfbae5b500c66f57a46da915912f3
MarioGranada <mario.hernandez@yolo.com>
2025-02-10 18:31:45 +0200

    feat: MDS-1543 add Group story

---
+++ docs/stories/Group.stories.tsx
@@ -0,0 +1,116 @@
+import { Group as GroupComponent, Hint } from "@heathmont/moon-core-tw";
+import type { Meta, StoryObj } from "@storybook/react";
+import getDefaultValues from "./utils/getDefaultValues";
+import { GenericInfo } from "@heathmont/moon-icons-tw";
+
+type GroupComponentProps = typeof GroupComponent;
+
+const options = ["sm", "md", "lg"] as const;
+type OptionsType = (typeof options)[number];
+const orientation = ["vertical", "horizontal"] as const;
+type OrientationType = (typeof orientation)[number];
+
+const defaultValues = {
+  className: "",
+  size: "md" as OptionsType,
+  orientation: "vertical" as OrientationType,
+  error: false,
+};
+
+const meta: Meta<GroupComponentProps> = {
+  component: GroupComponent,
+  title: "Moon DS/Group",
+  tags: ["autodocs"],
+  argTypes: {
+    className: {
+      description: "Additional CSS class for the Group.",
+      type: "string",
+    },
+    size: {
+      description: "Size of Group",
+      options,
+      control: { type: "select" },
+      table: {
+        type: { summary: options.join(" | ") },
+        defaultValue: { summary: "md" },
+      },
+    },
+
+    error: {
+      description: "Set valid/non-valid",
+      table: {
+        type: { summary: "boolean" },
+      },
+    },
+
+    orientation: {
+      description: "Two ways you can stack your input groups.",
+      options: orientation,
+      control: { type: "select" },
+      table: {
+        type: { summary: orientation.join(" | ") },
+        defaultValue: { summary: "vertical" },
+      },
+    },
+  },
+  render: ({ className, orientation, size, error }) => {
+    const rootProps = getDefaultValues(
+      { className, orientation, size },
+      defaultValues,
+    );
+
+    return (
+      <>
+        <GroupComponent {...rootProps} error={error}>
+          <GroupComponent.FirstInput placeholder="First input" />
+          <GroupComponent.LastInput placeholder="Last input" />
+        </GroupComponent>
+        {error && (
+          <Hint error>
+            <GenericInfo />
+            Informative message
+          </Hint>
+        )}
+        <GroupComponent {...rootProps}>
+          <GroupComponent.FirstSelect>
+            <option value="">First select</option>
+            <option value="1">Option 1</option>
+            <option value="2">Option 2</option>
+            <option value="3">Option 3</option>
+          </GroupComponent.FirstSelect>
+        </GroupComponent>
+        <GroupComponent {...rootProps}>
+          <GroupComponent.FirstInsetInput placeholder="First inset input" />
+          <GroupComponent.LastInsetInput placeholder="Last inset input" />
+          <GroupComponent.LastSelect>
+            <option value="">Last select</option>
+            <option value="1">Option 1</option>
+            <option value="2">Option 2</option>
+            <option value="3">Option 3</option>
+          </GroupComponent.LastSelect>
+        </GroupComponent>
+
+        <GroupComponent {...rootProps}>
+          <GroupComponent.FirstInsetSelect label="First inset select">
+            <option value="1">Option 1</option>
+            <option value="2">Option 2</option>
+            <option value="3">Option 3</option>
+          </GroupComponent.FirstInsetSelect>
+          <GroupComponent.LastInsetSelect label="Last inset select">
+            <option value="1">Option 1</option>
+            <option value="2">Option 2</option>
+            <option value="3">Option 3</option>
+          </GroupComponent.LastInsetSelect>
+        </GroupComponent>
+      </>
+    );
+  },
+};
+
+export default meta;
+
+type Story = StoryObj<GroupComponentProps>;
+
+export const Group: Story = {
+  args: defaultValues,
+};
