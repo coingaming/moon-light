f1a1864fea7180a81a5420693e8e36940bb83a54
MarioGranada <mario.hernandez@yolo.com>
2025-02-12 13:19:03 +0200

    feat: MDS-1545 add Icons story

---
+++ docs/stories/Icons.stories.tsx
@@ -0,0 +1,44 @@
+import type { Meta, StoryObj } from "@storybook/react";
+import {
+  OtherFrame,
+  ArrowsBoost,
+  ChatChat,
+  MailEnvelope,
+} from "@heathmont/moon-icons-tw";
+import getDefaultValues from "./utils/getDefaultValues";
+
+type IconProps = {
+  className?: string;
+};
+
+const defaultValues = { className: "" };
+
+const meta: Meta<IconProps> = {
+  title: "Moon DS/Icons",
+  tags: ["autodocs"],
+  argTypes: {
+    className: {
+      description: "Additional CSS class for the Icon.",
+      type: "string",
+    },
+  },
+  render: ({ className }) => {
+    const rootProps = getDefaultValues({ className }, defaultValues);
+    return (
+      <div className="inline-flex">
+        <OtherFrame {...rootProps} />
+        <ArrowsBoost {...rootProps} />
+        <ChatChat {...rootProps} />
+        <MailEnvelope {...rootProps} />
+      </div>
+    );
+  },
+};
+
+export default meta;
+
+type Story = StoryObj<IconProps>;
+
+export const Avatar: Story = {
+  args: defaultValues,
+};
