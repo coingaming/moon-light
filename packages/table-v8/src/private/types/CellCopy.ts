5bfb2df40bca4f796a149d98cc74dd7f3fe56668
Roberto Angulo <roberto.angulo@yolo.com>
2025-03-14 11:04:20 +0200

    fix: rename type [MDS-1664]

--- packages/table-v8/src/private/types/CellCopy.ts
+++ packages/table-v8/src/private/types/CellCopy.ts
@@ -1,6 +1,6 @@
 import { RefObject } from "react";
 
-export type CellCopyTextOuputType = {
+export type CellCopyTextOutputType = {
   wasCopiedSuccess: boolean;
   mouseEnterHandler: () => void;
   mouseLeaveHandler: () => void;
