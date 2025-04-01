c09638e2d8fefafb17aa1e71195a1a318960a9d8
robb91-dev <robb91-dev@users.noreply.github.com>
2025-04-01 14:33:40 +0000

    Prettified Code!

--- packages/core/src/tagsInput/TagsInput.tsx
+++ packages/core/src/tagsInput/TagsInput.tsx
@@ -253,7 +253,7 @@ const SelectedItem = ({
           onClick={onClick}
           className={mergeClassnames(
             selectedTagIndex === index
-              ? classNameTagOnFocus ?? "bg-piccolo"
+              ? (classNameTagOnFocus ?? "bg-piccolo")
               : "",
           )}
         >
