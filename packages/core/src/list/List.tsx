cf6bdd729290c0be7d94c1b2b21acd146daa577c
Roberto Angulo <roberto.angulo@yolo.com>
2025-04-04 10:22:23 +0300

    fix: remove justify-around [MDS-1744]

--- packages/core/src/list/List.tsx
+++ packages/core/src/list/List.tsx
@@ -2,21 +2,16 @@ import React from "react";
 import mergeClassnames from "../mergeClassnames/mergeClassnames";
 import { ListItemProps, ListProps } from "./private/types/ListProps";
 
-const ListRoot = ({ children, className }: ListProps) => {
+const ListRoot: React.FC<ListProps> = ({ children, className }) => {
   return <ul className={className}>{children}</ul>;
 };
 
-export const Item = ({ children, classNames }: ListItemProps) => {
+export const Item: React.FC<ListItemProps> = ({ children, classNames }) => {
   return (
-    <li
-      className={mergeClassnames(
-        "flex justify-around",
-        classNames?.itemContainer ?? "",
-      )}
-    >
+    <li className={classNames?.itemContainer}>
       <p
         className={mergeClassnames(
-          "w-100 flex justify-between border-b-1 border-bulma pt-1 pb-1 w-full",
+          "flex justify-between border-b-1 border-bulma pt-1 pb-1 w-full",
           classNames?.itemContent ?? "",
         )}
       >
@@ -26,7 +21,7 @@ export const Item = ({ children, classNames }: ListItemProps) => {
   );
 };
 
-export const ItemContent = ({ children, className }: ListProps) => {
+export const ItemContent: React.FC<ListProps> = ({ children, className }) => {
   return (
     <p className={mergeClassnames("flex items-center", className)}>
       {children}
