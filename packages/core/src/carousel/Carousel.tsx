4ed9fc158ef9f76dbd63d3e95f3683427f4f2490
MarioGranada <mario.hernandez@yolo.com>
2025-06-03 15:21:22 +0300

    fix: MDS-1673 fix alignment for few items in the carousel scenario

--- packages/core/src/carousel/Carousel.tsx
+++ packages/core/src/carousel/Carousel.tsx
@@ -132,7 +132,7 @@ const Reel = ({ children, className, ...rest }: SubcomponentProps) => {
         "[-ms-overflow-style:-ms-autohiding-scrollbar]",
         '[&>li]:list-none [&>li]:before:absolute [&>li]:before:content-["\\200B"]',
         "[&>*]:flex-[0_0_auto] [&>img]:h-full [&>img]:basis-auto [&>img]:w-auto",
-        "snap-x snap-mandatory rtl:flex-row-reverse",
+        "snap-x snap-mandatory justify-start",
         isSwipeDragDisabled && "overflow-x-hidden",
         className,
       )}
