62f1e27fac29a6b48d9f6cbae81f59ea5892c99b
Dmytro Kireiev <dmytro.kireiev@gmail.com>
2025-09-01 11:53:25 +0300

    fix: banner text

--- docs/components/betaBanner/BetaBanner.tsx
+++ docs/components/betaBanner/BetaBanner.tsx
@@ -2,7 +2,7 @@ import { ArrowsTopRight } from "@heathmont/moon-icons-tw";
 
 const BetaBanner = () => (
   <div className="sticky top-0 z-50 text-center px-4 py-1 text-moon-14 bg-roshi-60 backdrop-blur-xl">
-    Check out our new documentation website:{" "}
+    Check out the next generation of Moon DS:{" "}
     <a
       href="https://beta.moon.io"
       target="_blank"
